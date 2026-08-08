4a802c61d3e20648dae1ee5231fc0ba5483f351d
zixuan-come <zixuan0105@outlook.com>
2026-08-08 22:39:29 +0800

    chore: 初始化项目成员模型模块

---
+++ app/models/project_member.py
@@ -0,0 +1,8 @@
+from enum import Enum
+from sqlalchemy import CheckConstraint, Column, DateTime, ForeignKey, Integer, String, UniqueConstraint, func
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+
+
+
